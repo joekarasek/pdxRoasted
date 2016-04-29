cf7edc2fa841d8824df1552d8384f3ca462df0e6
jsonberry <jason.s.awbrey@gmail.com>
2016-04-28 21:14:34 -0700

    Add About component

---
+++ app/about.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from 'angular2/core';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'about',
+  template: `<h1>About</h1>`
+})
+
+export class AboutComponent {
+
+}
