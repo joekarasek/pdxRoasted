dab5cf3b164a27a4da951070274329634f9a91fd
jsonberry <jason.s.awbrey@gmail.com>
2016-04-28 21:14:15 -0700

    Add Contact component

---
+++ app/contact.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from 'angular2/core';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'contact',
+  template: `<h1>Contact</h1>`
+})
+
+export class ContactComponent {
+
+}
