cb33ab6d63a8ef9189ee92d62699a1cb6abf82cf
browneryan <browneryan87@gmail.com>
2016-04-28 21:00:43 -0700

    Import SearchResultListComponent into search.component.ts

--- app/search.component.ts
+++ app/search.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from 'angular2/core';
 import { Router } from 'angular2/router';
 import { FirebaseService } from './firebase.service';
 import { Observable } from 'rxjs/Observable';
+import { Roast } from './roast.model';
+import { SearchResultListComponent } from './search-result-list.component';
 
 @Component({
   selector: 'search',
+  inputs: ['roast'],
   template: `
     <label>Search</label>
     <input type="text" name="keyword">
@@ -12,5 +15,5 @@ import { Observable } from 'rxjs/Observable';
 })
 
 export class SearchComponent {
-
+  public roast: Roast;
 }
