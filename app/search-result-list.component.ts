a1c33557397f94682d20c48d88149193cd1112d2
browneryan <browneryan87@gmail.com>
2016-04-28 21:47:57 -0700

    Add search-result-list.component.ts basic structure

--- app/search-result-list.component.ts
+++ app/search-result-list.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from 'angular2/core';
 import { Router } from 'angular2/router';
 import { FirebaseService } from './firebase.service';
 import { Observable } from 'rxjs/Observable';
-import { SearchComponent } from './search.component';
+
+import { Roast } from './roast.model';
 
 @Component({
   selector: 'search-result-list',
-  template: `
-  `
+  templateUrl: 'app/search-result-list.component.html'
 })
 
-export class SearchResultListComponent { }
+export class SearchResultListComponent {
+  constructor(private _firebaseService: FirebaseService) {}
+}
