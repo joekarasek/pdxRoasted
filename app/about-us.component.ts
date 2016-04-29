f6ed4a94fc5491555308b49eea797b85af75a179
jsonberry <jason.s.awbrey@gmail.com>
2016-04-28 20:10:59 -0700

    Add AboutUs component skeleton and router link in App

---
+++ app/about-us.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
+import { FirebaseService } from './firebase.service';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'about',
+  template: `<h1>About Us</h1>`
+})
+
+export class AboutUsComponent {
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -6,18 +6,36 @@ import { HTTP_PROVIDERS } from 'angular2/http';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 //        (>'-')>  Firebase Service DI  <('-'<)
 import { FirebaseService } from './firebase.service';
+//        (>'-')>  Components  <('-'<)
 import { SearchComponent } from './search.component';
+import { AboutUsComponent } from './about-us.component';
 
 @Component({
   selector: 'my-app',
   template: `
       <h1>Hello, pdxRoasted!</h1>
+      <nav>
+        <ul>
+          <li>
+            <a [routerLink]="['AboutUs']">About Us</a>
+          </li>
+        </ul>
+      </nav>
+      <router-outlet></router-outlet>
   `,
   providers: [
     HTTP_PROVIDERS,
     FirebaseService
   ]
 })
+@RouteConfig([
+  {
+    path: '/aboutus',
+    name: 'AboutUs',
+    component: AboutUsComponent,
+
+  }
+])
 export class AppComponent {
 
 
