9b8798dca1af08e4d1486316ac1005163fa8146f
jsonberry <jason.s.awbrey@gmail.com>
2016-04-29 14:24:13 -0700

    Add ng2 Router to search-list comp

--- app/search-result-item.component.ts
+++ app/search-result-item.component.ts
@@ -2,16 +2,22 @@ import { Component, OnInit } from 'angular2/core';
 import { FirebaseService } from './firebase.service';
 import { Observable } from 'rxjs/Observable';
 
+import { Router } from 'angular2/router';
+
 @Component({
   selector: 'search-result-item',
   templateUrl: 'app/search-result-item.component.html'
 })
 
 export class SearchResultItemComponent implements OnInit{
-  constructor(private _firebaseService: FirebaseService) {}
+  constructor(
+    private _firebaseService: FirebaseService,
+    private _router: Router) {
+
+    }
 
   ngOnInit() {
-    
+
   }
 
 }
